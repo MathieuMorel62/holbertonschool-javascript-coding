54a77207b465d8f02eabe88db90ddcfc10b45d1e
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-10-16 12:21:21 +0200

    Update script to use request.get() and synchronous writing to file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,21 +1,14 @@
 #!/usr/bin/node
 
-const fs = require('fs');
 const request = require('request');
-
-const url = process.argv[2];
+const fs = require('fs');
+const apiURL = process.argv[2];
 const filePath = process.argv[3];
 
-request(url, (error, response, body) => {
+request.get(apiURL, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-      if (err) {
-        console.error('Erreur lors de l\'écriture du fichier:', err);
-      } else {
-        console.log('Contenu enregistré dans', filePath);
-      }
-    });
+    fs.writeFileSync(filePath, body, 'utf8');
   } else {
-    console.error('Erreur lors de la requête:', error);
+    console.error(error || `Erreur: Code d'état ${response.statusCode}`);
   }
 });
